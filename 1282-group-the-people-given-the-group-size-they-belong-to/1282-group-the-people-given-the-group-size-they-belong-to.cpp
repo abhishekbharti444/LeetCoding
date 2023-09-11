e3d1d17ffb6b7356d9d0f0382f98e1520f436e53
Abhishek Bharti <abharti444.ab@gmail.com>
2023-09-11 20:47:06 +0530

    Time: 12 ms (50.27%), Space: 13.9 MB (17.00%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        int n = groupSizes.size();
+        vector<vector<int>> res;
+        map<int, vector<int>> mp;
+        for (int i = 0; i < n; ++i) {
+            mp[groupSizes[i]].push_back(i);
+        }
+        
+        for (auto& el : mp) {
+            int count = el.first;
+            int total = el.second.size();
+            int k = total/count;
+            int index = 0;
+           
+            while (k--) {
+                vector<int> curr;
+                for (int i = 0; i < count; ++i) curr.push_back(el.second[index++]);
+                res.push_back(curr);
+            }
+        }
+        return res;
+    }
+};
