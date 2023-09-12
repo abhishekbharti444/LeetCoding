780eceb233cf765a022aa5acdbbf29e65d0e83a6
Abhishek Bharti <abharti444.ab@gmail.com>
2023-09-12 14:34:01 +0530

    Time: 75 ms (41.28%), Space: 17.5 MB (22.39%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,5 +1,17 @@
 class Solution {
 public:
+    /*
+        Approach:
+            1. Keep the counts of each characters in an unordered_map
+            2. Take a max_heap to have only the frequecies stored in descending order.
+            3. Compare top two elements only of the max_heap and if they are equal, add 1 to the res.
+            4. Decrease the top value by one and push it again to the max_heap to have processed for next iterations as well in case the top two were equal.
+            5. Each time only pop the top element(first). second is only used to compare with first.
+            
+            
+        Time: O(N + K^2 * logK); N => len of string, K => Maximum possible number of distinct chars in string.
+        Space: O(K)
+    */
     int minDeletions(string s) {
         int n = s.size();
         unordered_map<char, int> mp;
