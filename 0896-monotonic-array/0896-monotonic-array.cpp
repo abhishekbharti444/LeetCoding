1c589e8c1a99ef638498984804b42e6fce762baa
Abhishek Bharti <abharti444.ab@gmail.com>
2023-09-29 10:01:58 +0530

    Time: 122 ms (60.39%), Space: 97 MB (8.05%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -5,10 +5,8 @@ public:
         bool dec = true;
         int n = nums.size();
         for (int i = 1; i < n; ++i) {
+            if (nums[i] > nums[i-1]) dec = false;
             if (nums[i] < nums[i-1]) inc = false;
-        }
-        for (int i = n - 2; i >= 0; --i) {
-            if (nums[i] < nums[i+1]) dec = false;
         }
         return inc || dec;
     }
