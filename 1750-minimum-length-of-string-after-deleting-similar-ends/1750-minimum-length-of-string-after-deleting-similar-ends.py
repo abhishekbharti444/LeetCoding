002754a29d36d1aa8313e539b9d198422f92b36f
Abhishek Bharti <abharti444.ab@gmail.com>
2024-03-05 23:55:11 +0530

    Time: 74 ms (46.92%), Space: 17.3 MB (92.89%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -3,11 +3,16 @@ class Solution:
         n = len(s)
         left = 0
         right = n-1
-        while (left < right and s[left] == s[right]):
-            while (left < right and s[left] == s[left+1]):
+        while (left <= right):
+            ch_left = s[left]
+            ch_right = s[right]
+            if (left == right):
+                break
+            if (ch_left != ch_right):
+                return right - left + 1
+            while (left <= right and s[left] == ch_left):
                 left += 1
-            while (left < right and s[right] == s[right-1]):
+            while (left <= right and s[right] == ch_right):
                 right -= 1
-            left += 1
-            right -= 1
+        # print(left, s[left], right, s[right])
         return right - left + 1 if left <= right else 0
