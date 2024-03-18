11391ed5b76c66154c62bd0d9f70368512b08d83
Abhishek Bharti <abharti444.ab@gmail.com>
2024-03-18 22:11:55 +0530

    Time: 1032 ms (69.61%), Space: 62.8 MB (64.62%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,4 +1,19 @@
 class Solution:
+    
+    
+    """
+        The idea is to sort the array based on end time and count the other balloons can be burst for that end.
+            Approach : 
+                1. Sort the arr based on the end point.
+                2. If the start of the current balloon is smaller than the current end, increment count(burst the current balloon)
+                3. If not just update the end. Notice here we are not incrementing the count as this requires an arrow separately.
+                4. Count means the number of balloons for which we don't need a separate arrow.
+
+            Time : O(N);
+            Space : O(1);
+    """
+    
+    
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         points = sorted(points, key = lambda x: x[1])
         count = 0
