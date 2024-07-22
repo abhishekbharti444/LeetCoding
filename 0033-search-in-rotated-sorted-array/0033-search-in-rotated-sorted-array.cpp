e1e75ccb75bc7f6de0bc6ca07934dc5d9c8c8729
Abhishek Bharti <abharti444.ab@gmail.com>
2024-07-22 12:17:51 +0530

    Time: 5 ms (19.91%), Space: 14 MB (6.09%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    
+       /*
+        Approach :   If a sorted array is shifted, if you take the middle, always one side will be sorted. Take the recursion according to that rule.
+    
+        1- take the middle and compare with target, if matches return.
+        2- if middle is bigger than left side, it means left is sorted
+        2a- if [left] < target < [middle] then do recursion with left, middle - 1 (right)
+        2b- left side is sorted, but target not in here, search on right side middle + 1 (left), right
+        3- if middle is less than right side, it means right is sorted
+        3a- if [middle] < target < [right] then do recursion with middle + 1 (left), right
+        3b- right side is sorted, but target not in here, search on left side left, middle -1 (right)
+        
+    */ 
     int search(vector<int>& nums, int target) {
         int n = nums.size();
         int low = 0;
@@ -10,7 +23,7 @@ public:
             if (nums[mid] == target) return mid;
             if (target == nums[low]) return low;
             if (target == nums[high]) return high;
-            if (nums[mid] >= nums[low]) {
+            if (nums[mid] > nums[low]) {
                 if (target > nums[low] && target < nums[mid]) high = mid-1;
                 else low = mid+1;
             }
