1b7882035aef704c5b81db170773f4a529701d61
Abhishek Bharti <abharti444.ab@gmail.com>
2024-07-26 09:36:15 +0530

    Time: 263 ms (63.04%), Space: 122 MB (31.85%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,11 +1,43 @@
 class MedianFinder {
 public:
     
+    
     /*
         Approach : 
-        1. Put first half of the stream in a max_heap and second half in a min_heap as they would appear sorted in the stream.
-        2. To execute this minimally, always put the element in min_heap first, and then shift the top of min_heap to max_heap if required. 
-    */    
+            1. Take two heaps; min_heap, max_heap
+            2. min_heap will contain all the greater half elements and max_heap will contain the lower half elements. This is to facilitate the two middle elements at the top of min_heap and max_heaps
+            3. When trying to add an element, always add to exactly one heap for this code it's min_heap. And add the top of min_heap to the max_heap and pop from min_heap. 
+            
+                example :     max_heap [3, 2, 1]
+                              min_heap [8, 10, 12]
+                              median right now is 3 + 8 / 2
+                              num = 9
+                              
+                              After addition 
+                              max_heap [3, 2, 1]
+                              min_heap [8, 9, 10, 12]
+                              
+                              After max_heap population and min_heap popping
+                              max_heap [8, 3, 2, 1]
+                              min_heap [9, 10, 12]
+                              
+                              Since we always want to keep the size of the min_heap more in case of imbalance, we will do the push into min_heap once more of the top of the max_heap and popping it.
+                              max_heap [3, 2, 1]
+                              min_heap [8, 9, 10, 12]
+                              
+                              Now the median is 8.
+                              
+                              
+                              let's say num = 11
+                              After same steps the heaps will be
+                              max_heap [3, 2, 1]
+                              min_heap [8, 9, 10, 11, 12]
+                              
+                              max_heap [8, 3, 2, 1]
+                              min_heap [9, 10, 11, 12]
+                              
+                              New median would be 8 + 9 / 2
+    */ 
     priority_queue<int, vector<int>, greater<int>> min_heap;
     priority_queue<int> max_heap;
     
