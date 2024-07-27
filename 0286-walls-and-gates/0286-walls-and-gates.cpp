5747524f6cbd7dac01c90a9cc462a0f55ce12258
Abhishek Bharti <abharti444.ab@gmail.com>
2024-07-27 21:44:04 +0530

    Time: 24 ms (60.44%), Space: 17.2 MB (57.94%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.cpp
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
+    
+    const int inf = 2147483647;
+    
     vector<int> dx = {0, 0, -1, 1};
     vector<int> dy = {1, -1, 0, 0};
-    const int INF = 2147483647;
+    struct RowCol {
+        int row;
+        int col;
+    };
     bool isValid(int x, int y, int m, int n) {
         return x >= 0 && y >= 0 && x < m && y < n;
     }
+    
     void wallsAndGates(vector<vector<int>>& rooms) {
         int m = rooms.size();
         int n = rooms[0].size();
+        
         queue<pair<int, int>> q;
+        
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (rooms[i][j] == 0) {
-                    q.push({i, j});
-                }
+                if (rooms[i][j] == 0) q.push({i, j});
             }
         }
         
         while (!q.empty()) {
             int k = q.size();
             for (int i = 0; i < k; ++i) {
-                pair<int, int> curr = q.front();
+                auto top = q.front();
                 q.pop();
+        
                 for (int j = 0; j < 4; ++j) {
-                    int row = curr.first + dx[j];
-                    int col = curr.second + dy[j];
+                    int row = top.first + dx[j];
+                    int col = top.second + dy[j];
                     if (isValid(row, col, m, n)) {
                         if (rooms[row][col] == -1) continue;
-                        if (rooms[row][col] > 1 + rooms[curr.first][curr.second]) {
-                            rooms[row][col] = 1 + rooms[curr.first][curr.second];
+                        if (rooms[row][col] > 1 + rooms[top.first][top.second]) {
+                            rooms[row][col] = 1 + rooms[top.first][top.second];
                             q.push({row, col});
                         }
                     }
