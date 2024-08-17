03f33e8369c5eb156a46ff378c78a3c919d30624
Abhishek Bharti <abharti444.ab@gmail.com>
2024-08-17 23:14:25 +0530

    Time: 420 ms (32.60%), Space: 143.3 MB (25.44%) - LeetHub

---
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    
+    vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
+        sort(intervals.begin(), intervals.end());
+        unordered_map<int, int> res;
+        vector<int> Q = queries;
+        priority_queue<vector<int>> min_heap;
+        sort(Q.begin(), Q.end());
+        int i = 0;
+        int n = intervals.size();
+        for (int q : Q) {
+            while (i < n && intervals[i][0] <= q) {
+                int l = intervals[i][0];
+                int r = intervals[i][1];
+                min_heap.push({l - r - 1, r});
+                i++;
+            }
+            
+            while (!min_heap.empty() && q > min_heap.top()[1]) {
+                min_heap.pop();
+            }
+            
+            
+            res[q] = min_heap.empty() ? -1 : -min_heap.top()[0];
+        }
+        
+        vector<int> ans;
+        
+        for (int a : queries) {
+            ans.push_back(res[a]);
+        }
+        
+        return ans;
+    }
+};
