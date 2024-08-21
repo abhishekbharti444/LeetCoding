cc234e919b1700454c7088c753561282c6448ed5
Abhishek Bharti <abharti444.ab@gmail.com>
2024-08-21 13:35:36 +0530

    Time: 19 ms (22.29%), Space: 157.3 MB (6.93%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -2,11 +2,21 @@
 #define vvi vector<vi>
 class Solution {
 public:
-
-    
+    /*
+        Pretty straight forward DP. 
+        Approach: 
+            1. Two decisions are there to be made: 
+                1a. When we paste with the clipboard. clip will be same, curr will be curr + clip
+                1b. When we copy and paste. clip will become curr, curr will be curr + curr. 
+                1c. Notice how we are not doing just the copy operation since it will result into infinite recursion. 
+            2. Memoize the result with 2d vector. 
+            3. Edge case where n is 1 should have 0 as result. Since we have started the recursion with 2nd level where we pass the clip as 1 instead of 0 as that's the only way to go from the first level.
+            
+        Time: O(N^2)
+        Space: O(N^2)
+    */
     int n;
     vvi cache;
-    
     int util(int curr, int clip) {
         if (curr >= n) {
             if (curr == n) {
