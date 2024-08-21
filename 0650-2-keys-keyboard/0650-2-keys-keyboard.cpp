7ca4e4ee423192a9a90dbadb6bb190514046b91e
Abhishek Bharti <abharti444.ab@gmail.com>
2024-08-21 13:31:24 +0530

    Time: 19 ms (22.29%), Space: 157.3 MB (6.93%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,16 +1,27 @@
+#define vi vector<int>
+#define vvi vector<vi>
 class Solution {
 public:
+
+    
     int n;
-    vector<vector<int>> cache;
+    vvi cache;
+    
     int util(int curr, int clip) {
         if (curr >= n) {
-            if (curr == n) return 0;
+            if (curr == n) {
+                cache[curr][clip] = 0;
+                return 0;
+            }
             return 10001;
         }
-        return min(util(curr + clip, clip) + 1, util(curr + curr, curr) + 2);
+        if (cache[curr][clip] != -1) return cache[curr][clip];
+        
+        return cache[curr][clip] = min(util(curr + clip, clip) + 1, util(curr + curr, curr) + 2);
     }
     int minSteps(int n) {
         this->n = n;
+        cache = vvi (n + 1, vi (n + 1, -1));
         if (n == 1) return 0;
         return 1 + util(1, 1);     
     }
