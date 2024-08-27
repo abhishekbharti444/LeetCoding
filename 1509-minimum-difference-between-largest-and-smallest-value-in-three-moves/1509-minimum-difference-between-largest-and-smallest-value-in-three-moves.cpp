83e1e640d6bdefa1d4a7d1d5e7945ae8f0255818
Abhishek Bharti <abharti444.ab@gmail.com>
2024-08-27 21:09:31 +0530

    Time: 101 ms (5.65%), Space: 42.6 MB (5.42%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     int minDifference(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int res = INT_MAX;
+        
+        priority_queue<int, vector<int>, greater<int>> min_heap;
+        
+        priority_queue<int> max_heap;
+        
         int n = nums.size();
         if (n <= 3) return 0;
-        int left = 0;
-        int right = n - 4;
-        int res = INT_MAX;
-        while (right < n) {
-            res = min(res, nums[right] - nums[left]);
-            right++;
-            left++;
+
+        for (int i = 0; i < n; ++i) {
+            min_heap.push(nums[i]);
+            max_heap.push(nums[i]);
+            if (max_heap.size() > 4) max_heap.pop();
+            if (min_heap.size() > 4) min_heap.pop();
+        }
+        
+        priority_queue<int, vector<int>, greater<int>> new_heap;
+        while (!max_heap.empty()) {
+            new_heap.push(max_heap.top());
+            max_heap.pop();
+        }
+
+        while (!new_heap.empty()) {
+            res = min(res, abs(new_heap.top() - min_heap.top()));
+            new_heap.pop();
+            min_heap.pop();
         }
         return res;
     }
