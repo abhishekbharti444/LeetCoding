fa961b479096b22c9e1abe465348ff326190fb48
Abhishek Bharti <abharti444.ab@gmail.com>
2024-08-29 10:02:05 +0530

    Time: 172 ms (25.15%), Space: 76.5 MB (84.04%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.cpp
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    double averageWaitingTime(vector<vector<int>>& customers) {
-        int n = customers.size();
-        int curr_time = customers[0][0];
-        double wait_time = 0;
+    double averageWaitingTime(vector<vector<int>>& c) {
+        int m = c.size();
         
-        for (int i = 0; i < n; ++i) {
-            if (curr_time < customers[i][0]) {
-                curr_time = customers[i][0];
-            }
-            int curr_final_time = curr_time + customers[i][1];
-            curr_time = curr_final_time;
-            int curr_wait_time = curr_final_time - customers[i][0];
-
-            wait_time += curr_wait_time/(double)n;
-
+        long long total_time = 0;
+        int curr = c[0][0];
+        for (int i = 0; i < m; ++i) {
+            curr = max(curr, c[i][0]);
+            curr += c[i][1];
+            
+            total_time += curr - c[i][0];
+            cout<<curr<<" ";
         }
-        
-        return wait_time;
+        return (double)total_time/m;
     }
 };
