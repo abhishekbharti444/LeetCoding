20da048c84162fca39f5918af1dfc9cdddf96c6e
Abhishek Bharti <abharti444.ab@gmail.com>
2024-09-14 21:49:41 +0530

    Time: 2 ms (53.06%), Space: 7.5 MB (87.07%) - LeetHub

--- 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.cpp
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     
+    
+    /*
+        Solution inspired by lee215: https://leetcode.com/problems/minimum-addition-to-make-integer-beautiful/discuss/2758216/JavaC%2B%2BPython-Straight-Forward-Solution
+        
+        Approah: 
+            1. Write a function which can return the sum of digits
+            2. Have a value base which you can multiply later with the remaining of n
+            3. Finally return the value after multiplying with base and then subtracting with the original n;
+            
+        Time: O(logN * logN)
+    */
+    
     long long sumOfDigits(long long n) {
         long long res = 0;
         while (n > 0) {
