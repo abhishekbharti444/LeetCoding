faf1f9cc876a442b2143aa2f71fc51400a8400e2
Abhishek Bharti <abharti444.ab@gmail.com>
2025-09-07 18:16:23 +0530

    Time: 199 ms (87.74%), Space: 346.5 MB (90.33%) - LeetHub

--- 3863-power-grid-maintenance/3863-power-grid-maintenance.cpp
+++ 3863-power-grid-maintenance/3863-power-grid-maintenance.cpp
@@ -1,42 +1,75 @@
 class Solution {
 public:
+    /*
+     * Approach: Union-Find + Set for Online Station Tracking
+     * 
+     * 1. Use Union-Find to group stations into power grids (connected components)
+     * 2. Maintain sets of online stations for each grid component
+     * 3. For maintenance queries: find smallest online station in the same grid
+     * 4. For offline queries: remove station from its grid's online set
+     * 
+     * Key insight: Union by smaller ID ensures root has smallest ID in component
+     * 
+     * Time: O(α(c) * q), Space: O(c)
+     */
+    
     vector<int> parent;
+    
     int getParent(int a) {
         if (a == parent[a]) return a;
-        return parent[a] = getParent(parent[a]);
+        return parent[a] = getParent(parent[a]); // Path compression
     }
+    
     void unionMethod(int a, int b) {
         int pa = getParent(a);
         int pb = getParent(b);
+        // Union by smaller ID to maintain smallest root
         if (pa < pb) {
             parent[pb] = pa;
         }
         else parent[pa] = pb;
     }
+    
     vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
+        // Initialize Union-Find
         parent.resize(c + 1);
         for (int i = 0; i <= c; ++i) parent[i] = i;
-        vector<set<int>> vec(c + 1);
-        int con_len = connections.size();
-        for (int i = 0; i < con_len; ++i) {
-            unionMethod(connections[i][0], connections[i][1]);
+        
+        // Build power grids using Union-Find
+        for (auto& conn : connections) {
+            unionMethod(conn[0], conn[1]);
         }
-        for (int i = 0; i <= c; ++i) vec[getParent(i)].insert(i);
-        vector<int> res;
-        for (auto query: queries) {
-            if (query[0] == 1) {
-                int val = getParent(query[1]);
-                if (vec[val].empty()) res.push_back(-1);
-                else if (vec[val].find(query[1]) != vec[val].end()) res.push_back(query[1]);
-                else res.push_back(*vec[val].begin());
-            }
-            else {
-                int val = getParent(query[1]);
-                if (vec[val].find(query[1]) != vec[val].end()) {
-                    vec[val].erase(vec[val].find(query[1]));
+        
+        // Group online stations by their grid root
+        vector<set<int>> onlineStations(c + 1);
+        for (int i = 1; i <= c; ++i) {
+            onlineStations[getParent(i)].insert(i);
+        }
+        
+        vector<int> result;
+        
+        // Process each query
+        for (auto& query : queries) {
+            if (query[0] == 1) { // Maintenance check
+                int gridRoot = getParent(query[1]);
+                
+                if (onlineStations[gridRoot].empty()) {
+                    result.push_back(-1); // No online stations in grid
+                }
+                else if (onlineStations[gridRoot].count(query[1])) {
+                    result.push_back(query[1]); // Station is online
+                }
+                else {
+                    // Return smallest online station in grid
+                    result.push_back(*onlineStations[gridRoot].begin());
                 }
             }
+            else { // Station goes offline
+                int gridRoot = getParent(query[1]);
+                onlineStations[gridRoot].erase(query[1]);
+            }
         }
-        return res;
+        
+        return result;
     }
 };
