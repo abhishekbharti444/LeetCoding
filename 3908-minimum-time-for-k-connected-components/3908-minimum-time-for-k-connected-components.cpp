d1078723fd8cd55e7709a8ec085e10831606076c
Abhishek Bharti <abharti444.ab@gmail.com>
2025-09-07 18:42:11 +0530

    Time: 621 ms (23.45%), Space: 330.3 MB (32.85%) - LeetHub

---
+++ 3908-minimum-time-for-k-connected-components/3908-minimum-time-for-k-connected-components.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    unordered_map<int, vector<pair<int, int>>> g;
+    int n;
+    void dfsUtil(int node, vector<bool>& vis, int tm) {
+        if (vis[node]) return;
+        vis[node] = true;
+        for (auto [next_node, next_time]: g[node]) {
+            if (vis[next_node] == false && next_time > tm) {
+                dfsUtil(next_node, vis, tm);
+            }
+        }
+    }
+    bool getNumberOfComponents(int tm, int target_count) {
+        vector<bool> vis(n, false);
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            if (!vis[i]) {
+                dfsUtil(i, vis, tm);
+                count++;
+                // if (count >= target_count) return true;
+            }
+        }
+        cout<<"Count: "<<count<<endl;
+        return count >= target_count;
+    }
+    int minTime(int n, vector<vector<int>>& edges, int k) {
+        this->n = n;
+        int low = 0;
+        int high = 0;
+        for (auto edge: edges) {
+            g[edge[0]].push_back({edge[1], edge[2]});
+            g[edge[1]].push_back({edge[0], edge[2]});
+            high = max(high, edge[2]);
+        }
+        int res = -1;
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            bool check = getNumberOfComponents(mid, k);
+            cout<<"mid -> "<<mid<<"; Check => "<<check<<endl;
+            if (check) {
+                res = mid;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return res;
+    }
+};
