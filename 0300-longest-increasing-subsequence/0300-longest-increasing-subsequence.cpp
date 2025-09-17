d401e07db475ccfa15e7b34913f31edca2016dea
Abhishek Bharti <abharti444.ab@gmail.com>
2025-09-17 20:26:55 +0530

    Time: 0 ms (100%), Space: 14 MB (88.12%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    /*
+        Approach : Binary search(Awesome trick)
+            1. Take a vector which will contain the elements always in increasing order 
+                and when an element is encountered which is smaller than the last guy in this vector, 
+                we will replace one smaller element present in vec with this by doing the binary search of new element
+                in the existing vec elements.
+            2. If curr elem is greater than last elem of vec, we safely push_back in vec marking addition of length of LIS.
+            3. If curr elem is not greater than vec[last], we need to find curr suitable location
+                in the vec(doing binary search for curr in vec) and replace the old value with the curr value. 
+                This would work because we are taking a greater value than curr and 
+                making it smaller by replacing it with curr without breaking the increasing subsequence order.
+            4. At last return the size of the vec, as these are the numbers that form the LIS with lowest numbers possible.
+    */
     int lengthOfLIS(vector<int>& nums) {
         vector<int> vec;
         int n = nums.size();
