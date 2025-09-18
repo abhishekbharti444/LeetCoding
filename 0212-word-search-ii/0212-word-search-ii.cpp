2a94b187cb6ad7a1b61be934811faf5d2602e20f
Abhishek Bharti <abharti444.ab@gmail.com>
2025-09-18 07:41:24 +0530

    Time: 1585 ms (12.22%), Space: 21.4 MB (16%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -23,6 +23,14 @@ public:
         }
         return curr->is_word;
     }
+    bool hasPrefix(string prefix) {
+        TrieNode* curr = root;
+        for (char ch: prefix) {
+            if (!curr->next.contains(ch)) return false;
+            curr = curr->next[ch];
+        }
+        return true;
+    }
 
     unordered_set<string> res;
     int m, n;
@@ -32,13 +40,15 @@ public:
     bool isValid(int x, int y) {
         return x >= 0 && y >= 0 && x < m && y < n;
     }
-    void dfsUtil(int x, int y, string curr_str, vector<vector<bool>>& vis) {
-        if (!isValid(x, y) || vis[x][y]) return;
+    void dfsUtil(int x, int y, string curr_str, vector<vector<bool>>& vis, TrieNode* curr) {
+        if (!isValid(x, y) || vis[x][y] || !curr->next.contains(b[x][y])) return;
         vis[x][y] = true;
-        curr_str.push_back(b[x][y]);
-        if (search(curr_str)) res.insert(curr_str);
+        char ch = b[x][y];
+        curr_str.push_back(ch);
+        curr = curr->next[ch];
+        if (curr->is_word) res.insert(curr_str);
         for (int i = 0; i < 4; ++i) {
-            dfsUtil(x + dx[i], y + dy[i], curr_str, vis);
+            dfsUtil(x + dx[i], y + dy[i], curr_str, vis, curr);
         }
         curr_str.pop_back();
         vis[x][y] = false;
@@ -54,7 +64,7 @@ public:
             for (int j = 0; j < n; ++j) {
                 vector<vector<bool>> vis(m, vector<bool> (n, false));
                 string curr_str;
-                dfsUtil(i, j, curr_str, vis);
+                dfsUtil(i, j, curr_str, vis, root);
             }
         }
         vector<string> ans;
