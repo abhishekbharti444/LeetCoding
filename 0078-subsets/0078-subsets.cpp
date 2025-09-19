f55b0511bf83234b0b870ccd1bccfb6e65206035
Abhishek Bharti <abharti444.ab@gmail.com>
2025-09-19 09:51:21 +0530

    Time: 0 ms (100%), Space: 12.7 MB (27.69%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int n;
     vector<vector<int>> res;
-    vector<int> nums;
+    vector<int> A;
+    int n;
     void util(int index, vector<int>& comb) {
         if (index == n) {
             res.push_back(comb);
             return;
         }
-        comb.push_back(nums[index]);
+        util(index + 1, comb);
+        comb.push_back(A[index]);
         util(index + 1, comb);
         comb.pop_back();
-        util(index + 1, comb);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        n = nums.size();
-        this->nums = nums;
+        A = nums;
+        n = A.size();
         vector<int> comb;
         util(0, comb);
         return res;
