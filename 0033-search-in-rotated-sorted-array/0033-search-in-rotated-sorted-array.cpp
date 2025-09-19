410fd251adbed9b624cf0dd8ce0d1e0f8a16ef7e
Abhishek Bharti <abharti444.ab@gmail.com>
2025-09-19 12:39:09 +0530

    Time: 0 ms (100%), Space: 15.1 MB (65.87%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-    
-       /*
-        Approach :   If a sorted array is shifted, if you take the middle, always one side will be sorted. Take the recursion according to that rule.
-    
-        1- take the middle and compare with target, if matches return.
-        2- if middle is bigger than left side, it means left is sorted
-        2a- if [left] < target < [middle] then do recursion with left, middle - 1 (right)
-        2b- left side is sorted, but target not in here, search on right side middle + 1 (left), right
-        3- if middle is less than right side, it means right is sorted
-        3a- if [middle] < target < [right] then do recursion with middle + 1 (left), right
-        3b- right side is sorted, but target not in here, search on left side left, middle -1 (right)
-        
-    */ 
     int search(vector<int>& nums, int target) {
         int n = nums.size();
         int low = 0;
         int high = n-1;
-        
         while (low <= high) {
             int mid = low + (high - low)/2;
             if (nums[mid] == target) return mid;
-            if (target == nums[low]) return low;
-            if (target == nums[high]) return high;
+            if (nums[low] == target) return low;
+            if (nums[high] == target) return high;
+            // Left side is sorted
             if (nums[mid] > nums[low]) {
-                if (target > nums[low] && target < nums[mid]) high = mid-1;
-                else low = mid+1;
+                // Check if target in the left section
+                if (nums[low] < target && target < nums[mid]) 
+                    high = mid - 1;
+                else // If target is in the right side
+                    low = mid + 1;
             }
-            else {
-                if (target > nums[mid] && target < nums[high]) low = mid + 1;
-                else high = mid - 1;
+            else {// Right side is sorted
+                // Check if target is in the right section, go to the right side
+                if (nums[mid] < target && target < nums[high]) {
+                    low = mid + 1;
+                }
+                else high = mid - 1;// Browse in the left side.
             }
         }
         return -1;
