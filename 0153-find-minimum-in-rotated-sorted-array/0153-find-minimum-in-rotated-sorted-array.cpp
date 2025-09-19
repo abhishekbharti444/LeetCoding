ff386ac8b4fcc6d4cc958c528462ecd9b7163e30
Abhishek Bharti <abharti444.ab@gmail.com>
2025-09-19 12:50:45 +0530

    Time: 3 ms (1.26%), Space: 10.3 MB (100%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
+    /*
+        If I do a binary search; the condition to find the minimum would be in the right side
+            if elem at mid is greater than last element
+        it'll be in the left side 
+            if elem at mid is smaller than first element
+    */
     int findMin(vector<int>& nums) {
         int n = nums.size();
+        if (n == 1) return nums[0];
         int low = 0;
-        int high = n - 1;
-        int res = 0;
+        int high = n-1;
+        int index = 0;
         while (low <= high) {
             int mid = low + (high - low)/2;
-
-
-            if (nums[mid] > nums[mid + 1]) {// Check for a potential result
-                res = mid + 1;
+            
+            // Capture the index if two consecutive elements are out of order.
+            if (nums[mid] > nums[mid + 1]) {
+                index = mid + 1;
                 break;
             }
-            // Check if left side is sorted
+            
+            // Compare mid with the last elem, if nums[mid] is bigger, the result has to lie in the right side.
             if (nums[mid] > nums[n-1]) {
-                // The result should lie in the right side
                 low = mid + 1;
             }
-            else {// if right side is sorted
-                high = mid - 1; // The result must lie in the left side
-            } 
+            else {
+                high = mid - 1;
+            }
         }
-        return nums[res];
+        return nums[index];
     }
 };
