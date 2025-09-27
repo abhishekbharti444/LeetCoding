8a04e035118fbc0710c7397af6271c99e18191c8
Abhishek Bharti <abharti444.ab@gmail.com>
2025-09-27 11:17:58 +0530

    Time: 9 ms (17.79%), Space: 45.3 MB (87.24%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -2,22 +2,30 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
 
+        // Create dp table with same structure as triangle
         List<List<Integer>> dp = new ArrayList<>(Collections.nCopies(n, null));
 
+        // Process from bottom to top
         for (int i = n-1; i >= 0; --i) {
             int len = triangle.get(i).size();
+            // Copy current row from triangle to dp
             dp.set(i, new ArrayList<Integer>(triangle.get(i)));
 
             for (int j = 0; j < len; ++j) {
+                // Base case: bottom row stays as is
                 if (i == n-1) continue;
-                int curr = 0;
-                int right = 0;
-                right = dp.get(i + 1).get(j + 1);
-                curr = dp.get(i + 1).get(j);
-                int minVal = dp.get(i).get(j) + Math.min(right, curr);
+
+                // Two possible paths from current position
+                int curr = dp.get(i + 1).get(j);     // Path to (i+1, j)
+                int right = dp.get(i + 1).get(j + 1); // Path to (i+1, j+1)
+
+                // Update dp with minimum path sum
+                int minVal = dp.get(i).get(j) + Math.min(curr, right);
                 dp.get(i).set(j, minVal);
             }
         }
+
+        // Return minimum path sum from top
         return dp.get(0).get(0);
     }
 }
