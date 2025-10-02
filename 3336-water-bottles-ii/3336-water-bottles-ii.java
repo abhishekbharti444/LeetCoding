0b42b08e14967bcf5c0938f6456fb7ce4065b934
Abhishek Bharti <abharti444.ab@gmail.com>
2025-10-02 22:38:57 +0530

    Time: 1 ms (30.32%), Space: 40.8 MB (73.55%) - LeetHub

--- 3336-water-bottles-ii/3336-water-bottles-ii.java
+++ 3336-water-bottles-ii/3336-water-bottles-ii.java
@@ -1,17 +1,25 @@
 class Solution {
     public int maxBottlesDrunk(int numBottles, int numExchange) {
-        int empti = 0;
-        int res = 0;
-        while (numBottles > 0 || empti >= numExchange) {
-            res += numBottles;
-            empti += numBottles;
+        // Approach: Simulate the process - drink all bottles, then exchange empties
+        // Key insight: Exchange rate increases after each exchange operation
+        
+        int totalDrunk = 0;
+        int emptyBottles = 0;
+        
+        while (numBottles > 0 || emptyBottles >= numExchange) {
+            // Drink all available full bottles
+            totalDrunk += numBottles;
+            emptyBottles += numBottles;
             numBottles = 0;
-            if (empti >= numExchange) {
-                numBottles = 1;
-                empti -= numExchange;
-                numExchange++;
+            
+            // Exchange empties for one full bottle if possible
+            if (emptyBottles >= numExchange) {
+                numBottles = 1;                    // Get 1 full bottle
+                emptyBottles -= numExchange;       // Use required empties
+                numExchange++;                     // Exchange rate increases
             }
         }
-        return res;
+        
+        return totalDrunk;
     }
 }
